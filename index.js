91b0e3765f27eabaafbc5b430286e390c1e85c37
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2016-08-22 19:24:09 -0400

    v0.1.0

---
+++ index.js
@@ -0,0 +1,30 @@
+var rework = require('rework');
+var calc = require('rework-calc');
+var calc = require('rework-plugin-url');
+var vars = require('rework-vars')();
+var variant = require('rework-font-variant');
+var prefixes = require('autoprefixer')().rework;
+var customMedia = require('rework-custom-media');
+var limits = require('rework-ie-limits');
+var colors = require('rework-plugin-colors');
+
+module.exports = ClickCSS;
+
+@param {Object} stylesheet
+@param {Object} rework
+
+function ClickCSS (stylesheet, rework)
+	var css = rework(cssString)
+		.use(calc)
+		.use(vars())
+		.use(pseudos)
+    	.use(variant)
+    	.use(prefixes)
+    	.use(customMedia)
+    	.use(limits)
+    	.use(colors)
+		.use(rework.url(function(url){
+   	 		return url;
+  		}))
+		.toString();
+}
